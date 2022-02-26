6e18b259f7c1ed7f3e00696a50a352c6fe56acd1
charanvir <charanvir123@gmail.com>
2022-02-26 03:02:00 -0500

    highscore feature

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -128,11 +128,19 @@ var startGame = function () {
 var endGame = function () {
     window.alert("The game has now ended. Let's see how you did!");
 
-    // if player is still alive, player wins!
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + '.');
-    } else {
-        window.alert("You've lost your robot in battle!");
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " has set a new high score of " + playerInfo.money + "!");
+    }
+    else {
+        alert(playerInfo.name + " did not beat the high score of " + highScore + "!");
     }
 
     // ask player if they'd like to play again
